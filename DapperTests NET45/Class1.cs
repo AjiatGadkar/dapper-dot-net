8315dd907b33ba7c3dc8968cb423e714a8da0fa9
Sopam <sopanna87@gmail.com>
2014-01-14 16:22:12 +0530

    hello

---
+++ DapperTests NET45/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperTests_NET45
+{
+    class Class1
+    {
+    }
+}
